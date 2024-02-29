35b03b08ab244ed2fda0cffa3b46177e667455c2
Sebastian Valenzuela <svalenzuela.ortiz@gmail.com>
2024-02-29 16:34:30 -0300

    Estructura y Esquema sql

--- config/db/squema.sql
+++ config/db/squema.sql
@@ -1,3 +1,163 @@
 -- ************** creacion de tabla viajes **************
 -- Insertar aquí cuando se tenga listo
 -- ************** fin tabla usuarios **************
+
+
+-- ************** creacion de BASE DE DATOS **************
+
+CREATE DATABASE repuestos_automotriz;
+
+
+-- ************** creacion de tabla USUARIOS **************
+
+CREATE TABLE tbl_usuarios ( 
+id_usuario serial PRIMARY KEY, 
+nombre varchar(200),
+email varchar(100) NOT NULL, 
+password varchar(100) NOT NULL, 
+created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- ************** fin tabla USUARIOS **************
+
+-- ************** creacion de tabla PRODUCTOS **************
+
+CREATE TABLE tbl_productos (
+	id_producto SERIAL PRIMARY KEY,
+	SKU varchar(100),
+marca_producto varchar(100),
+	nombre varchar(150),
+	descripcion varchar(255),
+	precio_lista integer, 
+	stock integer,
+	usado boolean,
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+	
+);
+
+-- ************** fin tabla PRODUCTOS **************
+
+-- ************** creacion de tabla PRODUCTOS FAVORITOS **************
+
+CREATE TABLE tbl_productos_favoritos (
+	id_prod_favorito SERIAL PRIMARY KEY,
+	producto_id integer,
+	usuario_id integer,
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT fk_producto
+      FOREIGN KEY(producto_id) 
+        REFERENCES tbl_productos(id_producto),
+CONSTRAINT fk_usuario
+      FOREIGN KEY(usuario_id) 
+        REFERENCES tbl_usuarios(id_usuario)
+);
+-- ************** fin tabla PRODUCTOS FAVORITOS **************
+
+-- ************** creacion de tabla PRODUCTOS APLICACION **************
+
+CREATE TABLE tbl_productos_aplicacion (
+	id_producto_modelo SERIAL PRIMARY KEY NOT NULL,
+	producto_id integer,
+	marca varchar(100),
+	motor varchar(100),
+	modelo varchar(100),
+	cilindrada varchar(100),
+	agno_inicio integer,
+	agno_fin integer,
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT fk_prod_modelo
+      FOREIGN KEY(producto_id) 
+        REFERENCES tbl_productos(id_producto)
+);
+-- ************** fin tabla PRODUCTOS APLICACION **************
+
+
+-- ************** creacion de tabla IMAGENES **************
+
+CREATE TABLE tbl_imagenes (
+	id_img SERIAL PRIMARY KEY NOT NULL, 
+	producto_id integer,
+	url varchar(255),
+	orden integer,
+	estado integer,
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT fk_prod_img
+                   FOREIGN KEY(producto_id)
+		REFERENCES tbl_productos(id_producto)
+		 
+);
+
+-- ************** fin tabla IMAGENES **************
+
+
+-- ************** creacion de tabla PEDIDOS **************
+
+CREATE TABLE tbl_pedidos (
+	id_pedido SERIAL PRIMARY KEY NOT NULL,
+	usuario_id integer,
+	numero_pedido varchar(200),
+	observaciones varchar(200),
+	neto numeric,
+	iva numeric, 
+	total numeric,
+	estado varchar(100),
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT fk_usuario_pedido
+                   FOREIGN KEY(usuario_id)
+		REFERENCES tbl_usuarios(id_usuario)
+);
+
+-- ************** fin tabla PEDIDOS **************
+
+
+-- ************** creacion de tabla PEDIDOS DETALLE **************
+
+CREATE TABLE tbl_pedidos_detalle (
+	id_detalle SERIAL PRIMARY KEY NOT NULL,
+	pedido_id integer,
+	producto_id integer,
+	SKU varchar(200),
+	cantidad integer,
+	neto numeric,
+	iva numeric,
+	total numeric,
+	descuento numeric,
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT fk_pedido_id
+                   FOREIGN KEY(pedido_id)
+		REFERENCES tbl_pedidos(id_pedido),
+	CONSTRAINT fk_producto_pedido
+                   FOREIGN KEY(producto_id)
+		REFERENCES tbl_productos(id_producto)
+);
+
+-- ************** fin tabla PEDIDOS DETALLE **************
+
+
+-- ************** creacion de tabla PUBLICACIONES **************
+
+CREATE TABLE tbl_publicaciones(
+	id_publicacion SERIAL PRIMARY KEY NOT NULL,
+	usuario_id integer,
+	producto_id integer,
+	descripcion varchar(255),
+	estado integer,
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT fk_publicacion_usuario
+                   FOREIGN KEY(usuario_id)
+		REFERENCES tbl_usuarios(id_usuario),
+	CONSTRAINT fk_productos_publicacion
+                   FOREIGN KEY(producto_id)
+		REFERENCES tbl_productos(id_producto)
+);
+
+
+-- ************** fin tabla PUBLICACIONES **************
