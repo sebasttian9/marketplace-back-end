ed715e6a4483869b27b322e31968b190d0d9bac8
sebastian valenzuela <svalenzuela.ortiz@gmail.com>
2024-03-03 01:18:48 -0300

    add Login google, login normal, crear Usuarios

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ import express from "express";
 import cors from "cors";
 import { logger } from "logger-express";
 import swagger from "./config/swagger/swagger.js";
+import usersRouter from './config/routes/usersRoutes.js';
+
 
 /* Acá ir+an las rutas*/
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 swagger(app);
 app.use(express.json());
 app.options("*", cors());
 app.use(logger());
-/* app.use("/api/v1", [insertarRutas]); */
+app.use("/api/v1", usersRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
--- src/api/v1/models/usersModel.js
+++ src/api/v1/models/usersModel.js
@@ -1,16 +1,17 @@
-import pool from  "../../../../config/db/connections.js";
+import pool from  "../../../../config/db/conectionDb.js";
 import format from "pg-format";
 import bcrypt from "bcryptjs"; 
 
 
-const UserRegister = async (email,pass,rol, leng) => {
+const UserRegister = async (email,pass,nombre, authSource,imagen) => {
 
     try {
 
+      // Validar si el Usuario ya existe en la BD
         const passwordEncriptada = bcrypt.hashSync(pass)
         pass = passwordEncriptada
-        const values = [email, passwordEncriptada,rol,leng]
-        const consulta = "INSERT INTO usuarios (id,email,password,rol,lenguage) values (DEFAULT, $1, $2, $3, $4) RETURNING *"
+        const values = [nombre,email, passwordEncriptada,authSource,imagen]
+        const consulta = "INSERT INTO tbl_usuarios (id_usuario,nombre,email,password,authSource,avatar) values (DEFAULT, $1, $2, $3, $4, $5) RETURNING *"
         const response = await pool.query(consulta, values)
 
         return response.rows[0];
@@ -24,10 +25,10 @@ const UserRegister = async (email,pass,rol, leng) => {
 }
 
 
-const byEmail = async (email) => {
+const byEmail = async (email,authSource) => {
     const SQLquery = {
-      text: "SELECT * FROM usuarios WHERE email = $1",
-      values: [email],
+      text: "SELECT * FROM tbl_usuarios WHERE email = $1 and authSource = $2",
+      values: [email,authSource],
     };
     const response = await pool.query(SQLquery);
     return response.rows[0];
