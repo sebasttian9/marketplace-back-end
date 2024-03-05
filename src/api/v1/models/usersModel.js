05a605d1140dfc9c2c31fbac88efb9fce7a7ecf8
Rydozz15 <juan.fuentes@uc.cl>
2024-03-05 16:53:10 -0300

    Add Update User

--- src/api/v1/models/usersModel.js
+++ src/api/v1/models/usersModel.js
@@ -2,22 +2,39 @@ import pool from "../../../../config/db/conectionDb.js";
 import format from "pg-format";
 import bcrypt from "bcryptjs";
 
-const UserRegister = async (email, pass, nombre, authSource, imagenAvatar) => {
+const UserRegister = async (
+  email,
+  originalPassword,
+  name,
+  authSource,
+  imageAvatar
+) => {
   try {
     // Validar si el Usuario ya existe en la BD
-    const passwordEncriptada = bcrypt.hashSync(pass);
-    pass = passwordEncriptada;
-    const values = [
-      nombre,
+    const encryptPassword = bcrypt.hashSync(originalPassword);
+    const userRegisterValues = [
+      name,
       email,
-      passwordEncriptada,
+      encryptPassword,
       authSource,
-      imagenAvatar,
+      imageAvatar,
     ];
-    const consulta =
+    const userRegisterQuery =
       "INSERT INTO tbl_usuarios (id_usuario,nombre,email,password,authSource,avatar) values (DEFAULT, $1, $2, $3, $4, $5) RETURNING *";
-    const response = await pool.query(consulta, values);
+    const response = await pool.query(userRegisterQuery, userRegisterValues);
 
+    return response.rows[0];
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const UpdateUser = async (userId, name, email, password, imageAvatar) => {
+  try {
+    const updateUserValues = [name, email, password, imageAvatar, userId];
+    const updateUserQuery =
+      "UPDATE tbl_usuarios SET nombre = $1, email = $2, password = $3, avatar = $4 WHERE id_usuario = $5";
+    const response = await pool.query(updateUserQuery, updateUserValues);
     return response.rows[0];
   } catch (error) {
     console.log(error);
