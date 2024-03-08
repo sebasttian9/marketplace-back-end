c38e548e5f72fc3d08295104e8b648ab98b5ba2f
Rydozz15 <juan.fuentes@uc.cl>
2024-03-08 18:59:45 -0300

    Add a TO DO and :sku as a params

--- config/routes/productsRoutes.js
+++ config/routes/productsRoutes.js
@@ -12,7 +12,8 @@ const router = express.Router();
 router.get("/products", getAllProductsLimits);
 router.get("/products/:sku", getProductBySKU);
 router.post("/products", postNewProduct);
-router.put("/products", updateProduct);
+//TO DO: Se necesita un middleware que valide si el usuario es el creador o no
+router.put("/products/:sku", updateProduct);
 router.delete("/products/:sku", deleteProductBySku);
 
 export default router;
