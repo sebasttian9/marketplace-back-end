f02c8a8c281ac83afd303d4a9e24f4a2bb7959b3
Rydozz15 <juan.fuentes@uc.cl>
2024-03-08 15:55:26 -0300

    Add 4 new Product Controllers and their routes

--- config/routes/productsRoutes.js
+++ config/routes/productsRoutes.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { getAllProductsLimits } from '../../src/api/v1/controllers/productsControllers.js';
+import {
+  getAllProductsLimits,
+  postNewProduct,
+  getProductBySKU,
+  updateProduct,
+  deleteProductBySku,
+} from "../../src/api/v1/controllers/productsControllers.js";
 
 const router = express.Router();
 
-router.get('/products',getAllProductsLimits);
+router.get("/products", getAllProductsLimits);
+router.get("/products/:sku", getProductBySKU);
+router.post("/products", postNewProduct);
+router.put("/products", updateProduct);
+router.delete("/products/:sku", deleteProductBySku);
 
 export default router;
--- src/api/v1/controllers/productsControllers.js
+++ src/api/v1/controllers/productsControllers.js
@@ -1,31 +1,93 @@
-import { 
-    ProductRegister, 
-    bySKU, 
-    UpdateProductStock, 
-    DeleteProduct, 
-    getProducts } from '../models/productsModel.js';
-import prepareHateoas from '../helpers/hateoas.js';
+import {
+  ProductRegister,
+  bySKU,
+  UpdateEntireProduct,
+  DeleteProduct,
+  getProducts,
+} from "../models/productsModel.js";
+import { getSKU } from "../utils/utils.js";
+import prepareHateoas from "../helpers/hateoas.js";
 
+const getAllProductsLimits = async (req, res) => {
+  console.log(req.query);
 
+  try {
+    const { order_by, page, limits } = req.query;
+    const products = await getProducts(order_by, limits, page);
+    const productsWithHateoas = await prepareHateoas("products", products);
+    res.status(200).json(productsWithHateoas);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
-const getAllProductsLimits = async (req,res) => {
+const postNewProduct = async (req, res) => {
+  try {
+    const { brand, title, description, price, stock, state } = req.body;
+    const SKU = getSKU(title);
+    const newProduct = await ProductRegister(
+      SKU,
+      brand,
+      title,
+      description,
+      price,
+      stock,
+      state
+    );
+    //¿Coloco acá el producto del return?, ¿hace un return?
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
-    console.log(req.query)
+const getProductBySKU = async (req, res) => {
+  try {
+    //Tendría que ser onda "example.com/product/:sku" y sacamos el SKU por ahi
+    const { SKU } = req.params;
+    const ProductFoundByID = await bySKU(SKU);
+    //¿Coloco acá el producto del return?, ¿hace un return?
+    res.status(200).json(ProductFoundByID);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
-    try {
+const updateProduct = async (req, res) => {
+  try {
+    const { SKU, brand, title, description, price, stock, state } = req.body;
+    const updatedProduct = await UpdateEntireProduct(
+      SKU,
+      brand,
+      title,
+      description,
+      price,
+      stock,
+      state
+    );
+    //¿Coloco acá el producto del return?, ¿hace un return?
+    res.status(201).json(updatedProduct);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
-            const {order_by, page, limits} = req.query;
-            const products = await getProducts(order_by,limits,page);
-            const productsWithHateoas = await prepareHateoas("products", products);
-            res.status(200).json(productsWithHateoas);
+const deleteProductBySku = async (req, res) => {
+  try {
+    //Tendría que ser onda "example.com/product/:sku" y sacamos el SKU por ahi
+    const { SKU } = req.params;
+    const ProductDeleted = await DeleteProduct(SKU);
+    //¿Coloco acá el producto del return?, ¿hace un return?, ¿coloco algo?
+    res.status(204).send();
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
-        
-    } catch (error) {
-        console.log("error", error);
-    }
-    
-
-}
-
-
-export { getAllProductsLimits };
+export {
+  getAllProductsLimits,
+  postNewProduct,
+  getProductBySKU,
+  updateProduct,
+  deleteProductBySku,
+};
