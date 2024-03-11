f5d2705ad079c34f1b2d3081e7220c6143db9e8b
Sebastian Valenzuela <svalenzuela.ortiz@gmail.com>
2024-03-11 12:32:09 -0300

    productsRoute add function isLogin

--- config/routes/productsRoutes.js
+++ config/routes/productsRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getAllProductsLimits } from '../../src/api/v1/controllers/productsControllers.js';
+// import { isLogin } from "../../src/api/v1/middlewares/validaToken.js";
 
 const router = express.Router();
 
